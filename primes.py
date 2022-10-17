f4c15d9dc5ab3b3aa10e01d7abd2f8ae3c73dee8
G Allajmi <k20017434@kcl.ac.uk>
2022-10-17 17:11:37 +0100

    bababooey

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
     list = []
+    num = 2
+    while number_of_primes > 0:
+        isPrime = True
+        for potentialFactor in range(2, num):
+            if num % potentialFactor == 0:
+                isPrime = False
+                break
+        if isPrime:
+            number_of_primes -= 1
+            list.append(num)
+        num += 1
     return list
